2e4d8040e6f7cb632e8753aaf6c99a0e2ad1f4b9
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-07-12 10:42:21 +0800

    Add files via upload

---
+++ reactEmail.js
@@ -0,0 +1,44 @@
+// 2020, 7/12, am 10:30-
+// Email
+// CSS
+
+var fs = require("fs");
+var React = require("react");
+var EmailApp = React.createClass(
+
+    setInitailState: function(){
+        return(
+            {res: React.states.string}
+        );
+    },
+
+    render: function(){
+
+        var responseFile = this.state.res;
+        var styles;
+
+        if(responseFile){
+
+            // read path | url into Buffer
+            styles = <style>{fs.readFileSync(responseFile)}</style>;
+        }
+
+        return(
+
+            <html>
+
+                <body>
+
+                    {styles}
+                    {this.state.children}
+
+                </body>
+
+            </html>
+
+        );
+
+    }
+
+
+);
